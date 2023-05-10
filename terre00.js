33006e61c25c59f267a3ac6e3a907fd478ca9117
Simston <32360996+Simston@users.noreply.github.com>
2023-05-10 12:24:05 +0200

    Added Alphabet exercise

---
+++ terre00.js
@@ -0,0 +1,15 @@
+// EX 0 - ALPHABET
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var result = ""
+
+
+for (var a = 0; a < 26; a++) {
+
+    
+    result += alphabet[a];
+    
+
+}
+
+console.log(result);
