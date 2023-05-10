afd65a052efe80c539a3051f2a63f4eccf655510
Simston <32360996+Simston@users.noreply.github.com>
2023-05-10 15:54:15 +0200

    Version 1 of Program Name

--- terre01.js
+++ terre01.js
@@ -2,20 +2,61 @@
 
 // Recover Path of File with process.argv
 var pathOfFile = process.argv[1];
+// console.log(pathOfFile)
+
+var finalResult = "";
+
+// Recover lenght of string
+var lenghtS = 0;
+lenghtS = lenghtOfString(pathOfFile);
+//console.log(lenghtS)
 
 
-var char = pathOfFile[0];
-var stringResult = char;
-var numberOfChar = 0;
+// Example var i = 88; while 88 > 0 ; than 88 -1
+for (var i = lenghtS - 1; i >= 0; i--) {
 
-
-for (var i = 1; pathOfFile[i] != undefined; i++){
+    var char = pathOfFile[i];
+    // Variable to know the number of characters to display before the first slash
+    var afterLastSlash = i;
+    console.log(afterLastSlash)
     
-    char = pathOfFile[i];
+
+    if (char == "/"){
+        
+        // Delete / char for display file name
+        afterLastSlash += 1;
+        //console.log(afterLastSlash)
+
+        // x vaut 88  ; 88 > 78 ; x-1
+        for(var x = lenghtS; x > afterLastSlash; afterLastSlash++ ){
+
+            finalResult += pathOfFile[afterLastSlash];
+        
+        }
+
+        break;
+
+    } 
     
-    stringResult = stringResult + char;
-    numberOfChar++;
+    //console.log(char)
+
 
 }
-console.log(stringResult);
-console.log(numberOfChar); 
+console.log(finalResult)
+
+
+var stringResult;
+function lenghtOfString(string) {
+  
+    var numberOfChar = 0;
+
+    for (var i = 0; string[i] != undefined; i++){
+
+    numberOfChar++;
+
+    }
+
+    return numberOfChar;
+
+}
+
