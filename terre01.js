70931d056cb51b87872ff1d7dc8aca470b03f508
Simston <32360996+Simston@users.noreply.github.com>
2023-05-11 17:20:49 +0200

    Clean code

--- terre01.js
+++ terre01.js
@@ -1,52 +1,43 @@
 // EX 1 - Program Name
 
+var finalResult = "";
 // Recover Path of File with process.argv
 var pathOfFile = process.argv[1];
-// console.log(pathOfFile)
-
-var finalResult = "";
-
 // Recover lenght of string
 var lenghtS = 0;
-lenghtS = lenghtOfString(pathOfFile);
-//console.log(lenghtS)
+lenghtS = lenghtOf(pathOfFile);
 
 
-// Example var i = 88; while 88 > 0 ; than 88 -1
+//Just for me -- Example var i = 88; while 88 > 0 ; than 88 -1
 for (var i = lenghtS - 1; i >= 0; i--) {
 
     var char = pathOfFile[i];
-    // Variable to know the number of characters to display before the first slash
-    var afterLastSlash = i;
-    console.log(afterLastSlash)
-    
 
+    // New Variable to know the number of characters to display before the first slash
+    var afterLastSlash = i;
+    
     if (char == "/"){
         
         // Delete / char for display file name
         afterLastSlash += 1;
-        //console.log(afterLastSlash)
 
-        // x vaut 88  ; 88 > 78 ; x-1
+        // For me -- Example -- x vaut 88  ; 88 > 78 ; x-1
         for(var x = lenghtS; x > afterLastSlash; afterLastSlash++ ){
 
+            // final String after last Slash in $Path variable
             finalResult += pathOfFile[afterLastSlash];
         
         }
 
+        // Strop the program
         break;
 
     } 
-    
-    //console.log(char)
-
-
 }
 console.log(finalResult)
 
-
-var stringResult;
-function lenghtOfString(string) {
+//function for iterate over an String and return its lenght
+function lenghtOf(string) {
   
     var numberOfChar = 0;
 
