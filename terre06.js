f175b429311eaa469dae8c82a2d62493773bd08b
Simston <32360996+Simston@users.noreply.github.com>
2023-05-13 20:56:49 +0200

    "Completion of the inverted chain exercise"

--- terre06.js
+++ terre06.js
@@ -1,24 +1,45 @@
 // Inverser une chaîne
 
-// Recover first argument from command line
-var string = process.argv[2];
-//console.log(string)
+// Variable for verify all args
+var args = process.argv;
+var booleanVerif = verifyNumberOfArgument(args);
 
-var lenghtOfstring = lenghtOf(string)
-//console.log(lenghtOfstring)
+if(booleanVerif){
 
+    // Recover first argument from command line
+    var string = process.argv[2];
+    var lenghtOfstring = lenghtOf(string);
+    var inversedString = [];
 
-var inversedString = [];
+    for(var i = lenghtOfstring - 1; i >= 0; i--) {
 
-for(var i = lenghtOfstring - 1; i >= 0; i--) {
+        inversedString += string[i];
+    
+    }
 
-            inversedString += string[i];
-            //console.log("1");  
+    // Final Result
+    console.log(inversedString)
+
+}else{
+
+    console.log("Veuillez entrer une chaîne de caractère entre guillemets simples ''")
+
 }
 
+// Function to check if there is indeed a single argument passed
+function verifyNumberOfArgument(arg){
 
-console.log(inversedString)
+    //Count the number of Args passed in the command user's, and then iterate over the process array
+    var countArgs = lenghtOf(arg);
 
+    if ( countArgs > 3 || countArgs < 3) {
+    
+        return false;
+        
+    }else{
+        return true;
+    }
+}
 
 //function for iterate over a data and return its lenght
 function lenghtOf(data) {
@@ -26,7 +47,7 @@ function lenghtOf(data) {
     var count = 0;
 
     for (var i = 0; data[i] != undefined; i++){
-
+      
     count++;
 
     }
