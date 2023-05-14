46a2200de14fde5f9a4e5bc7bb0c3a825aff8bf2
Simston <32360996+Simston@users.noreply.github.com>
2023-05-14 14:28:40 +0200

    Added Ex 8 terre08.js Power of number

---
+++ terre08.js
@@ -0,0 +1,57 @@
+// Puissance d'un nombre
+
+// Variable for verify all args
+var args = process.argv;
+var booleanVerif = verifyNumberOfArgument(args);
+
+if(booleanVerif){
+
+    var base = process.argv[2];
+    var exposant = process.argv[3];
+    var result = 1;
+
+    for (var i = 0; i < exposant; i++) {
+
+        result *= base;
+        
+        //console.log(result)
+    }
+
+    if(result != 1){
+        console.log(result)
+       
+    }else{
+        console.log('Veuillez entrer une base et un exposant positif')
+    }
+}
+
+
+// Function to check if there is indeed a single argument passed
+function verifyNumberOfArgument(arg){
+
+    //Count the number of Args passed in the command user's, and then iterate over the process array
+    var countArgs = lenghtOf(arg);
+
+    if ( countArgs != 4) {
+    
+        return false;
+        
+    }else{
+        return true;
+    }
+}
+
+//function for iterate over a data and return its lenght
+function lenghtOf(data) {
+  
+    var count = 0;
+
+    for (var i = 0; data[i] != undefined; i++){
+      
+    count++;
+
+    }
+
+    return count;
+
+}
